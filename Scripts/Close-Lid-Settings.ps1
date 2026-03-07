b92b965fa7ace8c17d76ca164fdd51b9fd4c2843
chrishanr <chrishan.r@gmail.com>
2026-03-06 23:46:46 -0500

    Delete Scripts directory

--- Scripts/Close-Lid-Settings.ps1
+++
@@ -1,8 +0,0 @@
-#powercfg /hibernate on
-# Set Lid Close on Battery (DC) to Hibernate
-powercfg /setdcvalueindex SCHEME_CURRENT SUB_BUTTONS LIDACTION 3
-powercfg /setacvalueindex SCHEME_CURRENT SUB_BUTTONS LIDACTION 1
-
-# Force write + refresh
-powercfg /setactive SCHEME_CURRENT
-powercfg /qh > $null
