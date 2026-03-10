74a37edfc62f3163d5170a8a6bc43e79170e91b2
chrishanr <chrishan.r@gmail.com>
2026-03-09 21:46:35 -0400

    testing

--- Software-Outdated.ps1
+++ Software-Outdated.ps1
@@ -1,7 +1,7 @@
 # ============================
 # Last Software Update Checker
 # ============================
-
+#testing
 $WebhookUrl = "https://defaulteb6ac93710a44e86adf0ee412b4651.69.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/fbdbc04a4a6b466b912f6775ed0a021b/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=wibGRHpjHdWHWE50xS0_VdHtrrkdbjyeSdfKQseCpZU"
 $LastUpdateFile = "C:\DB\FILES\SOFTWAREUPDATE-LASTRUN.TXT"
 $LogPath = "C:\ProgramData\IT-Admin\DBSoftwareAudit.log"
