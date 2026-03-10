0bf7c78a04440bf8a87da14dbab2299f01d68b80
chrishanr <chrishan.r@gmail.com>
2026-03-09 23:40:19 -0400

    oudated software update

--- Software-Outdated.ps1
+++ Software-Outdated.ps1
@@ -4,7 +4,7 @@
 
 $WebhookUrl = "https://defaulteb6ac93710a44e86adf0ee412b4651.69.environment.api.powerplatform.com:443/powerautomate/automations/direct/workflows/fbdbc04a4a6b466b912f6775ed0a021b/triggers/manual/paths/invoke?api-version=1&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=wibGRHpjHdWHWE50xS0_VdHtrrkdbjyeSdfKQseCpZU"
 $LastUpdateFile = "C:\DB\FILES\SOFTWAREUPDATE-LASTRUN.TXT"
-$LogPath = "C:\ProgramData\IT-Admin\logs\DBSoftwareAudit.log"
+$LogPath = "C:\ProgramData\IT-Admin\logs\softwareupdate-lastrun.log"
 
 # --- Logging Function ---
 function Write-Log {
@@ -22,7 +22,7 @@ if (-not (Test-Path $LastUpdateFile)) {
 }
 
 # --- Read and parse the date ---
-$LastUpdateText = Get-Content $LastUpdateFile -Raw
+$LastUpdateText = (Get-Content $LastUpdateFile -Raw).Trim()
 
 # Example format: "Mon 02/23/2026 23:35:49.00"
 try {
