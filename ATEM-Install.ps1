7b4b1a18915e7e1bd85eb6922d459320c7a4a3ff
Chrishan Ratnasingham <chrishan.r@gmail.com>
2026-06-10 10:21:28 -0400

    download fix

--- ATEM-Install.ps1
+++ ATEM-Install.ps1
@@ -1,7 +1,7 @@
 # ================================
 # CONFIG
 # ================================
-$ZipUrl        = "https://swr.cloud.blackmagicdesign.com/ATEM/v10.2.1/Blackmagic_ATEM_Switchers_Windows_10.2.1.zip?verify=1780685836-JN6pnuLuZJ3noZp57%2F1E%2BEVVRSQSuGRkdSwf8BhClIA%3D"
+$ZipUrl        = "https://swr.cloud.blackmagicdesign.com/ATEM/v10.2.1/Blackmagic_ATEM_Switchers_Windows_10.2.1.zip?verify=1781101100-pm%2BWeqxcMEKY0ofq48JEdBUhKO5YxeuJXyCnQRbC%2Bwk%3D"
 $WorkDir       = "C:\DanceBUG\ESETDeploy"
 $ZipPath       = "$WorkDir\Blackmagic_ATEM_Switchers_Windows_10.2.1.zip"
 $InstallerPath = "$WorkDir\Blackmagic_ATEM_Switchers\Install ATEM v10.2.1.exe"
